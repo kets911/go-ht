c77ce3e59250722a7c209b6771a95b40aeab0adc
Pavel <kets911@users.noreply.github.com>
2019-03-01 11:20:22 +0300

    task-2 was done

--- task2/main.go
+++ task2/main.go
@@ -1,9 +1,41 @@
 package main
 
-// Specify MapTo function here
+func MapTo(arr []int, convert func(item, index int) string) (res []string) {
+	for i := 0; i < len(arr); i++ {
+		res = append(res, convert(arr[i], i))
+	}
+	return
+}
 
 func Convert(arr []int) []string {
-	// converting nimber code should be here
+	var result []string
+	for _, v := range arr {
+		var str string
+		switch v {
+		case 1:
+			str = "one"
+		case 2:
+			str = "two"
+		case 3:
+			str = "three"
+		case 4:
+			str = "four"
+		case 5:
+			str = "five"
+		case 6:
+			str = "six"
+		case 7:
+			str = "seven"
+		case 8:
+			str = "eight"
+		case 9:
+			str = "nine"
+		default:
+			str = "unknown"
+		}
+		result = append(result, str)
+	}
+	return result
 }
 
 func main() {
